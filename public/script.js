f5e7937ffedf780c5a0b76f9eeae81aa007a1a0b
CrossZeigler <70531290+CrossZeigler@users.noreply.github.com>
2020-11-07 15:00:37 -0500

    edited script.js, starting of typeahead, need to format to look better and to remove results when nothing is in the box
    Starting of typeahead, need to format CSS to make the output look nicer ,also need to format it so that results are removed (i.e. no results are shown) when the box is blank (even when deleting items)

---
+++ public/script.js
@@ -0,0 +1,90 @@
+
+
+    const endPoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+    const restaurants = [];
+
+    fetch(endPoint)
+    .then(blob => blob.json())
+    .then(data => restaurants.push(...data));
+
+
+  function findMatches(wordToMatch, restaurants) {
+
+    return restaurants.filter(place => {
+
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.name.match(regex) || place.state.match(regex);
+
+    })
+
+  }
+
+  function displayMatches() {
+
+    const matchArray = findMatches(this.value, restaurants);
+    const html = matchArray.map(place => {
+
+        return `
+        <li>
+            <span class="name">${place.name}, ${place.state}</span>
+            <span class="zip">${place.zip}</span>
+        <\li>
+        `;
+        
+
+    }).join('');
+    suggestions.innerHTML = html;
+
+  }
+
+      const searchInput = document.querySelector('.textinput');
+      const suggestions = document.querySelector('.flex-outer');
+
+      searchInput.addEventListener('change', displayMatches);
+      searchInput.addEventListener('keyup', displayMatches);
+
+
+
+
+
+  /*
+function typeAheadArray() {
+
+    const endPoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+
+    const restaurants = [];
+
+    fetch(endPoint)
+    .then(blob => blob.json())
+    .then(data => restaurants.push(...data));
+
+    return restaurants;
+
+  }
+
+  function findMatches(wordToMatch, restaurants) {
+
+    return restaurants.filter(place => {
+
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.name.match(regex) || place.state.match(regex);
+
+    })
+
+  }
+
+  function displayMatches() {
+
+    console.log(this.value);
+
+  }
+
+  function searchEvent() {
+      const searchInput = document.querySelector('.textinput');
+      const suggestions = document.querySelector('.flex-outer');
+
+      searchInput.addEventListener('change', displayMatches);
+
+  }
+  */
